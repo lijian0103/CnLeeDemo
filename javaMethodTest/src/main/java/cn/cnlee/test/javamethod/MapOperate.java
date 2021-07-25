2d7b6de18c7e9bae354c89b060fe2059f312c71f
cnlee <lijian0103@hotmail.com>
2021-07-25 11:17:36 +0800

    add map operate case

--- javaMethodTest/src/main/java/cn/cnlee/test/javamethod/MapOperate.java
+++ javaMethodTest/src/main/java/cn/cnlee/test/javamethod/MapOperate.java
@@ -2,13 +2,56 @@ package cn.cnlee.test.javamethod;
 
 import com.google.gson.Gson;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapOperate {
 
     public static void main(String[] args) {
+        testArrayContains();
+    }
+
+    private static void testArrayContains() {
+        int[] arr = new int[]{1,2,3,3};
+        System.err.println(Arrays.asList(arr).contains(2));
+        long xx = Arrays.stream(arr).filter(it -> it == 4).count();
+        System.err.println(xx);
+    }
+
+    private static void testFIFOMap() {
+        HashMap<Integer, Integer> map = new LinkedHashMap<Integer, Integer>() {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > 2;
+            }
+        };
+        map.put(3, 33);
+        map.put(1, 11);
+        map.put(2, 22);
+        System.err.println(map);
+        map.put(5, 55);
+        map.put(5, 88);
+        System.err.println(map);
+
+    }
+
+    private static void testMapSetVal() {
+        Map<Integer, Map<Integer, int[]>> map = new HashMap<>();
+        Map<Integer, int[]> childrenMap = new HashMap<>();
+        childrenMap.put(0, new int[]{1, 2, 3});
+        childrenMap.put(1, new int[]{5, 6, 7});
+        map.put(11, childrenMap);
+        childrenMap = new HashMap<>();
+        childrenMap.put(0, new int[]{4, 6, 8});
+        childrenMap.put(1, new int[]{3, 5, 7});
+        map.put(22, childrenMap);
+        System.err.println(new Gson().toJson(map));
+    }
+
+    private static void testMapUpdateKey() {
         Map<String, String> map = new HashMap<>();
         map.put("a", "1");
         map.put("b", "2");
@@ -31,7 +74,7 @@ public class MapOperate {
         //method three
         Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
-            Map.Entry<String,String> entry = iterator.next();
+            Map.Entry<String, String> entry = iterator.next();
             String key = entry.getKey();
             tempMap.put(key.toUpperCase(), map.get(key));
         }
@@ -41,5 +84,4 @@ public class MapOperate {
 
         System.err.println(new Gson().toJson(map));
     }
-
 }
