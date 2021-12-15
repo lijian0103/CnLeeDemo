c974704ed9a7cd7541d7a4a5c2f2521fb2917a88
cnlee <lijian0103@hotmail.com>
2021-12-15 23:44:24 +0800

    add flexbox manager case

--- databindingRecyclerview/src/main/java/cn/cnlee/demo/databindingrecyclerview/ui/adapter/CategoryAdapter.java
+++ databindingRecyclerview/src/main/java/cn/cnlee/demo/databindingrecyclerview/ui/adapter/CategoryAdapter.java
@@ -8,8 +8,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.JustifyContent;
 
 import java.util.List;
 
@@ -51,7 +55,17 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     public void onBindViewHolder(@NonNull CategoryAdapter.CategoryViewHolder holder, int position) {
         holder.binding.setVariable(BR.item, categoryList.get(position));
 
-        holder.binding.booksRv.setLayoutManager(new GridLayoutManager(this.context, 4));
+//        holder.binding.booksRv.setLayoutManager(new LinearLayoutManager(this.context, RecyclerView.HORIZONTAL, false));
+        FlexboxLayoutManager flexboxLayoutManager = new FlexboxLayoutManager(this.context);
+        //flexDirection 属性决定主轴的方向（即项目的排列方向）。类似 LinearLayout 的 vertical 和 horizontal。
+        flexboxLayoutManager.setFlexDirection(FlexDirection.ROW);//主轴为水平方向，起点在左端。
+        //flexWrap 默认情况下 Flex 跟 LinearLayout 一样，都是不带换行排列的，但是flexWrap属性可以支持换行排列。
+        flexboxLayoutManager.setFlexWrap(FlexWrap.WRAP);//按正常方向换行
+        //justifyContent 属性定义了项目在主轴上的对齐方式。
+        flexboxLayoutManager.setJustifyContent(JustifyContent.FLEX_START);//交叉轴的起点对齐。
+
+        holder.binding.booksRv.setLayoutManager(flexboxLayoutManager);
+
         BookAdapter bookAdapter = new BookAdapter(holder.binding.getRoot().getContext(), categoryList.get(position).getBooks());
         Log.d("CategoryAdapter", this.context + " === " + holder.binding.getRoot().getContext());
         holder.binding.booksRv.setAdapter(bookAdapter);
